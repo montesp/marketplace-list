9ec42f2d5cfe2d2307cac4b10c2524ab41e19fb6
Pavel Montes <pavelmontes22@gmail.com>
2023-01-31 21:31:55 -0600

    header css done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ function Header() {
     <header className='header'>
       <h3 className='header__title'>Market List</h3>
       <ul className='header__list'>
-        <li className='header__list-item'>Mi Lista</li>
-        <li className='header__list-item'>Inicio</li>
+        <li className='header__list-item'>My List</li>
+        <li className='header__list-item'>Home</li>
       </ul>
     </header>
   );
