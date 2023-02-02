a60203a9082aeb1c82994e60de0cb14aaca266f1
Pavel Montes <pavelmontes22@gmail.com>
2023-02-01 21:40:26 -0600

    footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer>
+      <h4>Created by Pavel </h4>
+      <ul>
+        <li><a href='#'>Github</a></li>
+        <li><a href='#'>Linkedin</a></li>
+        <li><a href='#'>Twitter</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export { Footer };
