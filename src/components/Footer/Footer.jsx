8639c216f630a78325519b19c45606fcc891fbb5
Pavel Montes <pavelmontes22@gmail.com>
2023-02-01 21:58:35 -0600

    footer css has done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import './Footer.scss';
 
 function Footer() {
   return (
-    <footer>
-      <h4>Created by Pavel </h4>
-      <ul>
+    <footer className='footer'>
+      <h4 className='footer__creator-title'>Created by Pavel </h4>
+      <ul className='footer__social-media-container'>
         <li><a href='#'>Github</a></li>
         <li><a href='#'>Linkedin</a></li>
         <li><a href='#'>Twitter</a></li>
