bc4d4ad246b20365417dc70bdbb1bb6b7b7954b9
Pavel Montes <pavelmontes22@gmail.com>
2023-02-25 23:33:07 -0600

    css responsive mobile first implemented on the UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,6 @@ function Header() {
   return (
     <header className='header'>
       <h3 className='header__title'>Market List</h3>
-      <ul className='header__list'>
-        <li className='header__list-item'>My List</li>
-        <li className='header__list-item'>Home</li>
-      </ul>
     </header>
   );
 }
